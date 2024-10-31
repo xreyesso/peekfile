cda2d042181b5866364a14762cedd95b40a11236
Xareni Reyes Soto <reyessoto.xareni@gmail.com>
2024-10-31 11:54:36 +0100

    Script modified to check if the file contains more than twice the input number of lines

--- peek.sh
+++ peek.sh
@@ -1,14 +1,14 @@
-#1. If user does not provide $2, it must default to 3
-if [[ $# -eq 1 ]]
+#2. If the input file contains more than $2*2 lines, show it completely.
+#Otherwise, show a warning and the first $2 and the last $2 lines
+NLINES=$(grep -c "" $1)
+if [[ $NLINES -le $(( $2 * 2 )) ]]
 then
-	head -n 3 $1
-	echo ...
-	tail -n 3 $1
-elif [[ $# -eq 2 ]]
-then
+	cat $1
+else
+	echo The file has more than 2*$2 = $(( $2 * 2 )) lines
 	head -n $2 $1
 	echo ...
 	tail -n $2 $1
-else
-	echo The number of arguments passed is not allowed
 fi
+
+
