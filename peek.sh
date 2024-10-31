a6b52bc9b8d857d0fd13d92e5a0b032242ab2ffc
Xareni Reyes Soto <reyessoto.xareni@gmail.com>
2024-10-31 11:07:09 +0100

    Script modified, if no number of lines is given, 3 is used

--- peek.sh
+++ peek.sh
@@ -1,3 +1,14 @@
-head -n $2 $1
-echo ...
-tail -n $2 $1
+#1. If user does not provide $2, it must default to 3
+if [[ $# -eq 1 ]]
+then
+	head -n 3 $1
+	echo ...
+	tail -n 3 $1
+elif [[ $# -eq 2 ]]
+then
+	head -n $2 $1
+	echo ...
+	tail -n $2 $1
+else
+	echo The number of arguments passed is not allowed
+fi
